ceddab24ffd65dc67e2e43128673a35cb34df721
Colbey Misico <comisico@gmail.com>
2022-02-21 12:21:09 -0500

    program update: Febuary 21, 2022

---
+++ Input_Logic.cpp
@@ -0,0 +1,8 @@
+//
+//  Input_Logic.cpp
+//  Chess
+//
+//  Created by Colbey Misico on 2/21/22.
+//
+
+#include <stdio.h>
---
+++ Input_Logic.h
@@ -0,0 +1,12 @@
+//
+//  Input_Logic.h
+//  Chess
+//
+//  Created by Colbey Misico on 2/21/22.
+//
+
+#ifndef Input_Logic_h
+#define Input_Logic_h
+
+
+#endif /* Input_Logic_h */
